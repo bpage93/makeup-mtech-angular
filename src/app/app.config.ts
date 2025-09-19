2854cf999744d46c84ce64f3ff91d84fafe36dad
bradpage93 <bradpage93@gmail.com>
2025-09-19 15:43:32 +0000

    fixed google login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { AuthService } from './Service/auth.service';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    AuthService,
+    provideAnimations(),
   ],
 };

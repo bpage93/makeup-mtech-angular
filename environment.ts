36569f83cc95aba0fbcbd64277e633077e9316bd
bpage93 <bradpage93@gmail.com>
2025-09-22 07:44:45 -0600

    added node version

---
+++ environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC2k0rPoLmxsxTj3MHyE1Ilq0LEuELpyyk',
+  authDomain: 'makeup-pro-2c722.firebaseapp.com',
+  projectId: 'makeup-pro-2c722',
+  storageBucket: 'makeup-pro-2c722.firebasestorage.app',
+  messagingSenderId: '859945662620',
+  appId: '1:859945662620:web:1fc1c6c9241825c848d559',
+  measurementId: 'G-QHT9JPN8LB',
+};
